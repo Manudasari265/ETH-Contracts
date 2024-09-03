9e029ebd0d8f3afa991a2994a1886e413f05b4c3
Manoj Dasari <dasarimanoj265@gmail.com>
2024-09-03 22:43:02 +0530

    mapping added

--- contracts/eth_contract.sol
+++ contracts/eth_contract.sol
@@ -6,7 +6,7 @@ contract SimpleStorage {
 
     // DATA TYPES IN SOLIDITY
     bool hasfavoriteNumber = true;
-    uint256 public favoriteNumber = 98;
+    uint256 myfavoriteNumber = 98;
 
     // uninitiazedNumber variable here gets initialized to 0 if no value is assigned
     uint256 uninitiazedNumber;
@@ -17,14 +17,42 @@ contract SimpleStorage {
     // 'bytes32' & 'bytes' are different types of data types
     bytes32 favoriteBytes32 = "cat";
 
-
+    // FUNCTIONS IN SOLIDITY
     // deployed contract address = 0xf8e81D47203A594245E36C48e151709F0C19fBe8
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myfavoriteNumber = _favoriteNumber;
     }
     
-    // view pure 
+    // view, pure. getter functions are created when 'public' keyword is used 
     function retrieve() public view returns(uint256) {
-        return favoriteNumber;
+        return myfavoriteNumber;
+    }
+
+    // ARRAYS AND STRUCTS
+    uint256[] listOfFavoriteNumbers;
+
+    struct Person {
+        uint256 favoriteNumber; // index 0
+        string name; // index 1
+    }
+
+    Person public manoj = Person({ favoriteNumber: 14, name: "Manoj" });
+    Person public sam = Person({ favoriteNumber: 24, name: "Sam" });
+    Person public raj = Person({ favoriteNumber: 44, name: "Raju" });
+
+    // above method is a tedious task, so we create array/list to contain all the list
+    // below is a dynamic array
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    // below is a static array
+    Person[5] public listOfPeople1; 
+
+    // function to add people to the dynamic array
+    // calldata, memory, storage
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push( Person(_favoriteNumber, _name) );
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
